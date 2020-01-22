492f1293c7ec46b158c5187a7ef708a97c710f23
yotaiyo <yota071218@gmail.com>
2020-01-23 00:05:58 +0900

    ColorfulEditorを作成

---
+++ src/ColorControls.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { styles, COLORS, colorStyleMap } from './consts';
+
+class StyleButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onToggle = (e) => {
+      e.preventDefault();
+      this.props.onToggle(this.props.style);
+    };
+  }
+
+  render() {
+    let style;
+    if (this.props.active) {
+      style = {...styles.styleButton, ...colorStyleMap[this.props.style]};
+    } else {
+      style = styles.styleButton;
+    }
+
+    return (
+      <span style={style} onMouseDown={this.onToggle}>
+        {this.props.label}
+      </span>
+    );
+  }
+}
+
+export const ColorControls = (props) => {
+    var currentStyle = props.editorState.getCurrentInlineStyle();
+    return (
+      <div style={styles.controls}>
+        {COLORS.map(type =>
+          <StyleButton
+            key={type.label}
+            active={currentStyle.has(type.style)}
+            label={type.label}
+            onToggle={props.onToggle}
+            style={type.style}
+          />
+        )}
+      </div>
+    );
+  };
--- src/ColorfulEditor.js
+++ src/ColorfulEditor.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import { EditorState } from 'draft-js';
+import { EditorState, Modifier, RichUtils } from 'draft-js';
 import { Editor } from 'draft-js';
+import { ColorControls } from './ColorControls';
+import { colorStyleMap, styles } from './consts';
 
 export class ColorfulEditor extends React.Component {
     constructor(props) {
-        super(props);
+      super(props);
+      this.state = {editorState: EditorState.createEmpty()};
 
-        this.state = { editorState: EditorState.createEmpty() }
-        this.onChange = editorState => this.setState({ editorState })
+      this.focus = () => this.editor.focus();
+      this.onChange = (editorState) => this.setState({editorState});
+      this.toggleColor = (toggledColor) => this._toggleColor(toggledColor);
+    }
+
+    _toggleColor(toggledColor) {
+      const {editorState} = this.state;
+      const selection = editorState.getSelection();
+
+        // Let's just allow one color at a time. Turn off all active colors.
+        const nextContentState = Object.keys(colorStyleMap)
+        .reduce((contentState, color) => {
+            return Modifier.removeInlineStyle(contentState, selection, color)
+        }, editorState.getCurrentContent());
+
+        let nextEditorState = EditorState.push(
+        editorState,
+        nextContentState,
+        'change-inline-style'
+        );
+
+        const currentStyle = editorState.getCurrentInlineStyle();
+
+        // Unset style override for current color.
+        if (selection.isCollapsed()) {
+        nextEditorState = currentStyle.reduce((state, color) => {
+            return RichUtils.toggleInlineStyle(state, color);
+        }, nextEditorState);
+        }
+
+        // If the color is being toggled on, apply it.
+        if (!currentStyle.has(toggledColor)) {
+        nextEditorState = RichUtils.toggleInlineStyle(
+            nextEditorState,
+            toggledColor
+        );
+        }
+
+        this.onChange(nextEditorState);
     }
 
     render() {
-        const { editorState } = this.state;
+        const {editorState} = this.state;
         return (
-            <>
-                <Editor
-                    editorState={editorState}
-                    onChange={this.onChange}
-                />
-            </>
-        )
+        <div style={styles.root}>
+            <ColorControls
+            editorState={editorState}
+            onToggle={this.toggleColor}
+            />
+            <div style={styles.editor} onClick={this.focus}>
+            <Editor
+                customStyleMap={colorStyleMap}
+                editorState={editorState}
+                onChange={this.onChange}
+                placeholder="Write something colorful..."
+                ref={(ref) => this.editor = ref}
+            />
+            </div>
+        </div>
+        );
     }
 }
