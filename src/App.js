e28b4cd292830e65b193a655c4e99a9a29e87171
yotaiyo <yota071218@gmail.com>
2020-01-22 21:46:36 +0900

    draft-jsを導入

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Editor, EditorState } from 'draft-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+
+    this.onChange = (editorState) => {
+      this.setState({ editorState });
+    }
+  }
+
+  render() {
+    const { editorState } = this.state
+
+    return (
+      <>
+        <Editor
+          editorState={editorState}
+          onChange={this.onChange}
+        />
+      </>
+    )
+  }
 }
 
-export default App;
+export default MyEditor;
